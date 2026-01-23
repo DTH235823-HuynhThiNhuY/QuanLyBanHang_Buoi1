9bbf296fc9a4c5fd3c09cedea89e808ca6bc1e28
Admin <Admin@MT0323>
2026-01-23 09:59:26 +0700

    Add project files.

---
+++ QuanLyBanHang/Data/QLBHDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Configuration;
+
+namespace QuanLyBanHang.Data
+{
+    public class QLBHDbContextFactory : IDesignTimeDbContextFactory<QLBHDbContext>
+    {
+        public QLBHDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<QLBHDbContext>();
+
+            // Đọc từ App.config
+            var connectionString = ConfigurationManager.ConnectionStrings["QLBHConnection"]?.ConnectionString;
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string 'QLBHConnection' not found in App.config");
+            }
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new QLBHDbContext(optionsBuilder.Options);
+        }
+    }
+}
