3ad6119b782d49fa333624b83a51b567da41c11b
HuynhThiNhuY <y_dth235823@student.agu.edu.vn>
2026-02-02 20:21:28 +0700

    sua file

---
+++ QuanLyBanHang/Migrations/20260202131156_KhoiTaoCSDL.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLyBanHang.Migrations
+{
+    /// <inheritdoc />
+    public partial class KhoiTaoCSDL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HoaDon_NhanViens_NhanVienID",
+                table: "HoaDon");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NhanViens",
+                table: "NhanViens");
+
+            migrationBuilder.RenameTable(
+                name: "NhanViens",
+                newName: "NhanVien");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NhanVien",
+                table: "NhanVien",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HoaDon_NhanVien_NhanVienID",
+                table: "HoaDon",
+                column: "NhanVienID",
+                principalTable: "NhanVien",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HoaDon_NhanVien_NhanVienID",
+                table: "HoaDon");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NhanVien",
+                table: "NhanVien");
+
+            migrationBuilder.RenameTable(
+                name: "NhanVien",
+                newName: "NhanViens");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NhanViens",
+                table: "NhanViens",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HoaDon_NhanViens_NhanVienID",
+                table: "HoaDon",
+                column: "NhanVienID",
+                principalTable: "NhanViens",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
